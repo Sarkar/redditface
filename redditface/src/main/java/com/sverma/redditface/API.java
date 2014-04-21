94bc2db2db4915f693b4036a5d4cdd90707ef9c8
Sarkar <sahilverma1@gmail.com>
2014-04-20 22:58:32 -0700

    Add API and AuthActivity for reddit logins

---
+++ redditface/src/main/java/com/sverma/redditface/API.java
@@ -0,0 +1,14 @@
+package com.sverma.redditface;
+
+
+import com.squareup.okhttp.OkHttpClient;
+
+/**
+ * Created by SVerma on 4/20/14.
+ *
+ * Class to Auth against Reddit's servers.
+ * Used in AuthActivity.java
+ */
+public class API {
+    private static final String BaseURL = "http://reddit.com";
+}
